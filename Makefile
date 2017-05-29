b0136cdc5d1a2b65604c5b371b579352dfabfd16
Finlay Maguire <finlaymaguire@gmail.com>
2017-05-29 12:52:54 -0300

    Speed up lat long code using lookup table

--- Makefile
+++ Makefile
@@ -2,19 +2,14 @@ SEQ=test.fasta
 
 all: metadata.csv $(SEQ).phy
 
-metadata.csv: $(SEQ)
+metadata.csv: $(SEQ) ./scripts/strip_data.sh ./scripts/add_lat_long.py
 	@echo "Generating metadata file"
 	./scripts/strip_data.sh $(SEQ) > metadata_tmp.csv
-	./scripts/add_lat_long.py metadata_tmp.csv 2 > metadata.csv
-	rm metadata_tmp.csv
+	./scripts/add_lat_long.py metadata_tmp.csv 1 > metadata.csv
+	-rm metadata_tmp.csv
 	
-$(SEQ).phy: $(SEQ).mask $(SEQ).model
-	@echo "ML Tree"
-	raxmlHPC-SSE3 -f a -p 42 -x 42 -# 100 -s $(SEQ).mask -m $(cat $(SEQ).model) -n $(SEQ).phy
-
-$(SEQ).model: $(SEQ).mask
-	@echo "Model Fitting"
-	./prottest3/dist/prottest3 -i $(SEQ).mask -F -BIC > $(SEQ).model
+$(SEQ).contree: $(SEQ).mask
+	iqtree-omp -s $(SEQ).mask -abayes -bb 10000 -alrt 0 -nt 2
 
 $(SEQ).mask: $(SEQ).afa
 	@echo "Masking"
@@ -26,5 +21,8 @@ $(SEQ).afa: $(SEQ)_clean.faa
 
 $(SEQ)_clean.faa: $(SEQ)
 	@echo "Cleaning Accessions"
-	awk -F '_' '/^>/ {print $1}; !/^>/ {print}' $(SEQ) > $(SEQ)_clean.faa
+	awk -F '_' '/^>/ {print $$1}; !/^>/ {print}' $(SEQ) > $(SEQ)_clean.faa
+
+clean:
+	-rm $(SEQ)_clean.faa $(SEQ).* metadata.csv
 
--- scripts/add_lat_long.py
+++ scripts/add_lat_long.py
@@ -1,55 +1,60 @@
-#!/usr/bin/env python                                                                                                           1 ↵
+#!/usr/bin/env python
 """
 Quick script to add lat/long to metadata
 """
-
-from geopy.geocoders import Nominatim
+import pandas as pd
+import os
 import argparse
+import urllib.request
+import zipfile
 
-def get_lat_long(country, geolocator):
-    """
-    Use geolocator to get lat/long from country name
-
-    Params
-    ------
-    country: str
-        country name and or address
-
-    geolocator: geopy.geocoders
-        initialised geopy geolocation class
+def get_centroids():
+    '''
+    Grab Centroids CSV if it doesn't exist and parse into
+    a dataframe
 
     Returns
     -------
 
-    lat: float
-        latitude of country
-    long: flat
-        lontitude of country
-    """
-    location = geolocator.geocode(country)
-    return location.latitude, location.longitude
+    centroids: pd.DataFrame
+        DataFrame of parsed centroids data
+    '''
+    centroids_fp = 'country_centroids_primary.csv'
+    if not os.path.exists(centroids_fp):
+        url = 'http://gothos.info/resource_files/country_centroids.zip'
+        urllib.request.urlretrieve(url, 'country_centroids.zip')
+        with zipfile.ZipFile('country_centroids.zip') as fh_z:
+            fh_z.extract(centroids_fp)
+        os.unlink('country_centroids.zip')
 
-
+    centroids = pd.read_csv(centroids_fp, sep='\t')
+    return centroids
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Add lat and long to metadata')
-    parser.add_argument('csv', help='Path to CSV')
-    parser.add_argument('field', help='Field containing country', type=int)
+    parser.add_argument('metadata', help='Path to metadata CSV')
+    parser.add_argument('field', help='Field in metadata CSV with sampling loc',
+                        type=int)
     args = parser.parse_args()
 
-    geolocator = Nominatim()
+    centroids = get_centroids()
 
-    with open(args.csv) as fh:
+    with open(args.metadata) as fh:
+
         # write header
         header = next(fh).strip()
-        print(header)
 
         for line in fh:
             line = line.strip().split(',')
-            loc = get_lat_long(line[args.field], geolocator)
-            line = line + list(map(str, loc))
+            country = line[args.field]
+            print(country)
+            data = centroids[centroids['SHORT_NAME'] == country]
+            if len(data) == 1:
+                line.append(str(data['LAT'].item()))
+                line.append(str(data['LONG'].item()))
+            else:
+                raise ValueError('Country not found: {}'.format(country))
             print(",".join(line))
-
 
